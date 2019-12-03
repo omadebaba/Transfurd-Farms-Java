12f4bb790d61cc9bc2604c4258ce251c1daaab03
omadebaba <iamoyemadeteepee@gmail.com>
2019-12-03 15:46:07 +0100

    Project Set-up

---
+++ src/test/java/com/project/transfurdfarms/TransfurdFarmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.transfurdfarms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransfurdFarmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
